cfd168c6c0fe309e5b0716a4f13b7b3a1b4ec1c0
aToshik <aa.nooker@gmail.com>
2022-10-25 16:43:06 +0300

    init setup

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 25 Oct 2022 13:37:51 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
